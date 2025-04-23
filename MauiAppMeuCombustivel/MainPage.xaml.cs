da13801a32fc25c93cf211d658b52e943ee12378
Pablo Vinícius <pblo.vvi11@gmail.com>
2025-04-23 11:29:54 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMeuCombustivel/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace MauiAppMeuCombustivel
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                double etanol = Convert.ToDouble(txt_etanol.Text);
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+
+                string msg = "";
+
+                if (etanol <= (gasolina * 0.7))
+                {
+                    msg = "Etanol é a melhor opção";
+                }
+                else
+                {
+                    msg = "Gasolina é a melhor opção";
+                }
+
+                DisplayAlert("Calculado", msg, "Ok");
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "Fechar");
+            }
+        } //fecha metodo
+    } //fecha class
+
+} //fecha main space
